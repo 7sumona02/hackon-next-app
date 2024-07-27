e60a8155f1dd3e47e143d2f469cf33bdc50fcaa8
7sumona02 <hawkingsumona8@gmail.com>
2024-07-27 15:18:09 +0530

    minor changes

--- components/MentorCard.tsx
+++ components/MentorCard.tsx
@@ -7,8 +7,8 @@ interface MentorCardProps {
 
 const MentorCard: React.FC<MentorCardProps> = ({speaker, companyImg}) => {
   return (
-    <div className='bg-white w-36 h-56 rounded-lg p-2'>
-      <div className='h-36 overflow-hidden'>
+    <div className='bg-white w-44 h-64 rounded-lg p-2'>
+      <div className='h-44 overflow-hidden'>
         <img src='/m1.jpg' className='w-full h-full object-cover rounded-xl' />
       </div>
       <div className='flex flex-col gap-1 mt-2'>
--- components/Mentors.tsx
+++ components/Mentors.tsx
@@ -1,6 +1,7 @@
 import { Pixelify_Sans } from 'next/font/google';
 import React from 'react'
 import MentorCard from './MentorCard';
+import Image from 'next/image';
 
 const pixel = Pixelify_Sans({
     weight: '700',
@@ -10,11 +11,12 @@ const pixel = Pixelify_Sans({
 
 const Mentors = () => {
   return (
-    <div className='mt-14 pb-10'>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className={`${pixel.className} text-4xl text-center`}>SPEAKERS <br /> & MENTORS</h1>
+    <div className='mt-28 pb-10'>
+      <div className='flex flex-col justify-center items-center relative'>
+        <Image src='/Atom.svg' alt='img' height={100} width={100} className='absolute -rotate-12 -top-16 left-14' />
+        <h1 className={`${pixel.className} text-4xl text-center z-40`}>SPEAKERS <br /> & MENTORS</h1>
 
-        <div className='flex flex-col gap-4 justify-center items-center pb-10 pt-10'>
+        <div className='pb-10 pt-10'>
             <div className='grid grid-cols-2 gap-4'>
                 <MentorCard speaker='NAVIN GUPTA' companyImg='/company/Apple.svg' />
                 <MentorCard speaker='XYZ' companyImg='/company/Apple.svg' />
